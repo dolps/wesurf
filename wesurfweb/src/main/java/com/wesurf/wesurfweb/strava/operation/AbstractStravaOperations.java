ae84e87392a047eebb7935f752436f2e6e497c26
dolplads@gmail.com <broCode123>
2018-05-28 15:24:29 +0200

    minor refactoring

--- wesurfweb/src/main/java/com/wesurf/wesurfweb/strava/operation/AbstractStravaOperations.java
+++ wesurfweb/src/main/java/com/wesurf/wesurfweb/strava/operation/AbstractStravaOperations.java
@@ -8,17 +8,20 @@ import org.springframework.util.MultiValueMap;
 import java.net.URI;
 
 public class AbstractStravaOperations {
-	private final boolean isAuthorized;
+    private final boolean isAuthorized;
+    private static final String STRAVA = "strava";
+    private static final String API_URL_BASE = "https://www.strava.com/api/v3";
+    private static final LinkedMultiValueMap<String, String> EMPTY_PARAMETERS = new LinkedMultiValueMap<String, String>();
 
-	public AbstractStravaOperations(boolean isAuthorized) {
-		this.isAuthorized = isAuthorized;
-	}
+    public AbstractStravaOperations(boolean isAuthorized) {
+        this.isAuthorized = isAuthorized;
+    }
 
-	protected void requireAuthorization() {
-		if (!isAuthorized) {
-			throw new MissingAuthorizationException("strava");
-		}
-	}
+    protected void requireAuthorization() {
+        if (!isAuthorized) {
+            throw new MissingAuthorizationException(STRAVA);
+        }
+    }
 
     protected URI buildUri(String path) {
         return buildUri(path, EMPTY_PARAMETERS);
@@ -27,9 +30,4 @@ public class AbstractStravaOperations {
     protected URI buildUri(String path, MultiValueMap<String, String> parameters) {
         return URIBuilder.fromUri(API_URL_BASE + path).queryParams(parameters).build();
     }
-
-	// Strava API v3
-	private static final String API_URL_BASE = "https://www.strava.com/api/v3";
-
-    private static final LinkedMultiValueMap<String, String> EMPTY_PARAMETERS = new LinkedMultiValueMap<String, String>();
 }
